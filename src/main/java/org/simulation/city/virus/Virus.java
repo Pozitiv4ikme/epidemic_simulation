b973e3c86367766dd287b711950b619b7b97efdc
Pozitiv4ikme <vitaliy.pashynskyi@gmail.com>
2025-05-18 15:44:11 +0200

    initialized City class and all of all dependent classes

---
+++ src/main/java/org/simulation/city/virus/Virus.java
@@ -0,0 +1,11 @@
+package org.simulation.city.virus;
+
+public class Virus {
+    private int infectionProbability;
+    private int mutationStage;
+    private int lethality;
+
+    public Virus mutation() {
+        return new Virus();  // stub
+    }
+}
